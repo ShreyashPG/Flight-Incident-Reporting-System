fb1b07e7398090b1b43ae21f11a11bcee0e2e7e1
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-06-19 00:57:18 +0530

    added api base url at backend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: process.env.REACT_APP_API_BASE_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
